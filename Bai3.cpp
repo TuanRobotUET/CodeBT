0e9ea5e695025adffd498a583c5c36ed286d9415
TuanRobotUET <24022924@vnu.edu.vn>
2024-09-22 14:28:08 +0700

    ADD file

---
+++ Bai3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+using namespace std;
+int main() {
+    int nam;
+    cout<<"Nam hien tai la bao nhieu : ";
+    cin>>nam;
+    cout<<"Xin chao"<<endl;
+    cout<<"Ban dang hoc lap trình C vao nam "<<nam;
+    return 0;
+}
